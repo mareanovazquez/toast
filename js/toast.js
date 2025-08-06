7ebb0061a261332a69d9609879b6f2cdf9fac6a8
mareanovazquez <mareanovazquez@protonmail.com>
2025-08-06 14:57:14 -0300

    mejora en la composición de cada toast

--- js/toast.js
+++ js/toast.js
@@ -1,28 +1,35 @@
+/* Toast Simple */
+function initSimpleToast() {
+    let triggerToast = document.getElementById('triggerToast');
 
-let triggerToast = document.getElementById('triggerToast');
+    triggerToast.addEventListener('click', function () {
+        let toast = document.getElementById('toast');
+        toast.classList.add('visible');
 
-triggerToast.addEventListener('click', function () {
-    let toast = document.getElementById('toast');
-    toast.classList.add('visible');
+        setTimeout(function(){
+            toast.classList.remove('visible');
+        }, 3000);
+    });
+}
 
-    setTimeout(function(){
-        toast.classList.remove('visible');
-    },3000)
-    
-})
+/* Toast con Barra de Progreso */
+function initProgressToast() {
+    let triggerToast2 = document.getElementById('triggerToast2');
 
+    triggerToast2.addEventListener('click', function () {
+        let toast2 = document.getElementById('toast2');
+        let barToast2 = document.getElementById('barToast2');
 
-let triggerToast2 = document.getElementById('triggerToast2');
+        toast2.classList.add('visible');
+        barToast2.style.width = '100%';
 
-triggerToast2.addEventListener('click', function () {
-    let toast2 = document.getElementById('toast2');
-    let barToast2 = document.getElementById('barToast2');
+        setTimeout(function () {
+            toast2.classList.remove('visible');
+            barToast2.style.width = '0%';
+        }, 3000);
+    });
+}
 
-    toast2.classList.add('visible');
-    barToast2.style.width = '100%';
-
-    setTimeout(function () {
-        toast2.classList.remove('visible');
-        barToast2.style.width = '0%';
-    }, 3000);
-});
+// Inicializar los toasts
+initSimpleToast();
+initProgressToast();
